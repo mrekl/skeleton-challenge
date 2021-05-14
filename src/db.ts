d5b1bbe30d6f217f438b8b4d7c8baa72b22ebd3f
Marcin Reklinski <kontakt@reklinski.pl>
2021-05-14 13:43:34 +0200

    Fixed: db access key

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ import * as AWS from 'aws-sdk';
 dotenv.config();
 
 const region = process.env.AWS_DEFAULT_REGION;
-const accessKeyId = process.env.AKIAQHYC3IBFBY6WYYEL;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 export const db = (): any => {
